a562e7762399c422777c89ff2cf7e8e3b2d0d3a0
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-07-29 11:04:59 +0200

    Get release stats through GitHub REST API v3

---
+++ scripts/release_download_count.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab-related actions.
+#
+# SPDX-License-Identifier: MIT
+"""Script to fetch the download count of each release in a GitHub repository."""
+
+import sys
+import typing
+
+import requests
+
+
+def release_download_count(
+    repository_name: str, token: str, per_page: int
+) -> typing.Dict[typing.Tuple[str, str], int]:
+    """Fetch the download count of each release in a GitHub repository."""
+    if token != "":
+        headers = {"Authorization": f"token {token}"}
+    else:
+        headers = None
+    # Get all releases and corresponding download count
+    page = 1
+    count = dict()
+    while True:
+        response = requests.get(
+            f"https://api.github.com/repos/{repository_name}/releases?page={page}&per_page={per_page}",
+            headers=headers).json()
+        assert isinstance(response, list)
+        if len(response) > 0:
+            for release in response:
+                release_name = release["name"]
+                for asset in release["assets"]:
+                    asset_name = asset["name"]
+                    asset_download_count = asset["download_count"]
+                    assert (release_name, asset_name) not in count
+                    count[(release_name, asset_name)] = asset_download_count
+            page += 1
+        else:
+            break
+    # Return a dictionary containing (release name, release asset): download count
+    return count
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 4
+    release_download_count(sys.argv[1], sys.argv[2], int(sys.argv[3]))
---
+++ tests/test_release_download_count.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab-related actions.
+#
+# SPDX-License-Identifier: MIT
+"""Tests for the release_download_count script."""
+
+import importlib
+import os
+import sys
+import types
+
+import pytest
+
+
+@pytest.fixture
+def root_directory() -> str:
+    """Return the root directory of the repository."""
+    return os.path.dirname(os.path.dirname(__file__))
+
+
+@pytest.fixture
+def release_download_count(root_directory: str) -> types.ModuleType:
+    """Load the release_download_count module."""
+    sys.path.insert(0, os.path.join(root_directory, "scripts"))
+    release_download_count = importlib.import_module("release_download_count")
+    sys.path.pop(0)
+    return release_download_count
+
+
+def test_release_download_count(release_download_count: types.ModuleType) -> None:
+    """
+    Test the release_download_count function on the current repository.
+
+    This test will fail in the (unlikely) case of a release asset being downloaded between the
+    first and second "release_download_count" call.
+    """
+    dict_many_per_page = release_download_count.release_download_count(
+        "fem-on-colab/release-download-count-script", "", 10)
+    dict_one_per_page = release_download_count.release_download_count(
+        "fem-on-colab/release-download-count-script", "", 1)
+
+    assert dict_many_per_page == dict_one_per_page
+    assert ("Initial commit", "test_file.txt") in dict_many_per_page
+    assert dict_many_per_page[("Initial commit", "test_file.txt")] > 0
+    assert ("Initial commit (copy)", "test_file_2.txt") in dict_many_per_page
+    assert dict_many_per_page[("Initial commit (copy)", "test_file_2.txt")] > 0
+    assert ("Initial commit (copy)", "test_file_3.txt") in dict_many_per_page
+    assert dict_many_per_page[("Initial commit (copy)", "test_file_3.txt")] > 0
